a4786c3971ea6fcb615bb0283a2979fea3b9779e
Anmol Patel <120417559+Anmolpatel562@users.noreply.github.com>
2024-03-10 14:34:46 +0530

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import NoteList from "./components/NoteList";
+import NoteNoteSelected  from './components/NoteNoteSelected';
+import Lock from './image/lock.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NoteList/>
+     <div className='noteScreen'>
+       <NoteNoteSelected/>
+       <div className='security'>
+        <img className='lockImg' src={Lock}></img>
+        <p>end-to-end encrypted</p>
+        </div>
+     </div>
     </div>
   );
 }
---
+++ src/components/NoteList.jsx
@@ -0,0 +1,18 @@
+import "./NoteList.css"
+
+import React from 'react'
+
+function NoteList() {
+  return (
+    <div>
+      <div className="noteListContainer">
+        <h1 className="appName">Pocket Notes</h1>
+        <div className="noteList">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NoteList;
